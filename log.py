8185268132b23ad45eb1231bf8fef9c99eb09827
ivanhercaz <ivanhercaz@gmail.com>
2018-02-03 23:58:46 +0000

    specifying parameters in methods and minor changes

--- log.py
+++ log.py
@@ -1,35 +1,43 @@
 # -*- coding: utf-8 -*-
 import datetime, inquirer, logging, os
 
-def update(fullNameLog):
+def update(info, fullNameLog, dateTime):
+    print(fullNameLog)
     print("\nActualizando log...")
+
     logging.basicConfig(
         filename = fullNameLog,
         level = logging.INFO,
         format = "%(asctime)s » %(message)s",
         datefmt = "%d/%m/%Y %I:%M:%S %p"
     )
+    logging.info(info)
 
-def check(key, script):
+    print(dateTime + " » " + info)
+
+def check(info, key, script):
     logDir = "logs/"
     now = datetime.datetime.now()
-    nowFormat = now.strftime("%Y-%m-%d_%H:%M")
+    nowFormat = now.strftime("%Y-%m-%d %H:%M")
 
     if key == "":
         nameLog = script
     else:
-        nameLog = key + "-" + script
+        nameLog = "-" + key + "-" + script
 
-    fullNameLog = logDir + nowFormat + "-" + nameLog
+    fullNameLog = logDir + nowFormat + nameLog + ".log"
 
     if os.path.exists(logDir):
-        update(fullNameLog)
+        update(info, fullNameLog, nowFormat)
     else:
         print(logDir + " creado")
         os.makedirs(logDir)
-        update(fullNameLog)
+        update(info, fullNameLog, nowFormat)
 
-def ask(script = ""):
+def ask(info="", script = "script desconocido"):
+    # Ternary operation to make a default info text if there isn't info provided
+    info = "No se ha aportado información sobre lo ocurrido" if (info == "") else info
+
     nameQuestion = [
         inquirer.List("nameLog", message = u"¿Quieres añadirle alguna palabra clave al nombre del log?",
                         choices = [u"Sí", u"No"])
@@ -37,6 +45,7 @@ def ask(script = ""):
 
     answers = inquirer.prompt(nameQuestion)
 
+    # Check if it's necessary some extra keyword to identify the log file
     if answers["nameLog"] == u"Sí":
         nameQuestion = [
             inquirer.Text("keyName", message = "¿Qué palabra clave?")
@@ -47,8 +56,11 @@ def ask(script = ""):
         keyName = answers["keyName"]
 
         print(u"\nSe utilizará el siguiente nombre: año-mes-día_hora:minutos-" + keyName + "-" + script + ".log")
-        check(keyName, script)
+        check(info, keyName, script)
     else:
-        check()
+        # There isn't any keyname, so just enter two args
+        check(info, key = "", script = "")
 
+# This is here to test the module by default, without the necessity to make changes in any script file.
+# When the module will be fine tested and ready to use in the scripts, this will be deleted.
 ask()
