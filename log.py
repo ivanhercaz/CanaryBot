efdd2e1fe2afecda9ff4e3c430109cec39968269
ivanhercaz <ivanhercaz@gmail.com>
2018-02-04 18:45:34 +0000

    method ask() deleted to simplify the module; minor changes and comments

--- log.py
+++ log.py
@@ -2,30 +2,28 @@
 import datetime, inquirer, logging, os
 
 def update(info, fullNameLog, dateTime):
-    print(fullNameLog)
     print("\nActualizando log...")
 
     logging.basicConfig(
         filename = fullNameLog,
         level = logging.INFO,
-        format = "%(asctime)s » %(message)s",
+        format = u"%(asctime)s » %(message)s",
         datefmt = "%d/%m/%Y %I:%M:%S %p"
     )
     logging.info(info)
 
-    print(dateTime + " » " + info)
+    print(dateTime + u" » " + info)
 
-def check(info, key, script):
+    return fullNameLog, dateTime
+
+def check(info, script):
+    # Checking format of the name log
+
     logDir = "logs/"
     now = datetime.datetime.now()
     nowFormat = now.strftime("%Y-%m-%d %H:%M")
 
-    if key == "":
-        nameLog = script
-    else:
-        nameLog = "-" + key + "-" + script
-
-    fullNameLog = logDir + nowFormat + nameLog + ".log"
+    fullNameLog = logDir + nowFormat + script + ".log"
 
     if os.path.exists(logDir):
         update(info, fullNameLog, nowFormat)
@@ -34,33 +32,6 @@ def check(info, key, script):
         os.makedirs(logDir)
         update(info, fullNameLog, nowFormat)
 
-def ask(info="", script = "script desconocido"):
-    # Ternary operation to make a default info text if there isn't info provided
-    info = "No se ha aportado información sobre lo ocurrido" if (info == "") else info
-
-    nameQuestion = [
-        inquirer.List("nameLog", message = u"¿Quieres añadirle alguna palabra clave al nombre del log?",
-                        choices = [u"Sí", u"No"])
-    ]
-
-    answers = inquirer.prompt(nameQuestion)
-
-    # Check if it's necessary some extra keyword to identify the log file
-    if answers["nameLog"] == u"Sí":
-        nameQuestion = [
-            inquirer.Text("keyName", message = "¿Qué palabra clave?")
-        ]
-
-        answers = inquirer.prompt(nameQuestion)
-
-        keyName = answers["keyName"]
-
-        print(u"\nSe utilizará el siguiente nombre: año-mes-día_hora:minutos-" + keyName + "-" + script + ".log")
-        check(info, keyName, script)
-    else:
-        # There isn't any keyname, so just enter two args
-        check(info, key = "", script = "")
-
 # This is here to test the module by default, without the necessity to make changes in any script file.
 # When the module will be fine tested and ready to use in the scripts, this will be deleted.
-ask()
+# ask()
