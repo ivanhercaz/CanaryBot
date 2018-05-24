902beaf5d310d8dcd292ceeeb1ed14bdafb9fe34
ivanhercaz <ivan@ivanhercaz.com>
2018-05-25 00:50:46 +0100

    add logs to logs.html to be listed in logslist.html

--- createhtml.sh
+++ createhtml.sh
@@ -9,6 +9,8 @@
 FULLNAMELOG=$1
 # CSV viewer
 HTMLFILE=$2
+# File to list all the logs
+LOGLIST="logs/logs.html"
 # Name of the script-task made
 TASK=$3
 
@@ -20,7 +22,6 @@ cat > $HTMLFILE << _EOF_
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>CSV to HTML Table</title>
     <meta name="author" content="Iván Hernández Cazorla">
 
@@ -29,7 +30,6 @@ cat > $HTMLFILE << _EOF_
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/dataTables.bootstrap.css" rel="stylesheet">
   </head>
-
   <body>
     <div class="container-fluid">
 
@@ -38,46 +38,47 @@ cat > $HTMLFILE << _EOF_
 
       <div id='table-container'></div>
 
-    </div><!-- /.container -->
+</div><!-- /.container -->
 
-    <footer class='footer'>
-      <div class='container-fluid'>
-        <hr />
-        <p class='pull-right'>Thank you very much to <a href='http://derekeder.com'>Derek Eder</a>,
-        creator of <a href='https://github.com/derekeder/csv-to-html-table'>CSV to HTML Table</a>,
-        who released it under <a href="https://github.com/derekeder/csv-to-html-table/blob/master/LICENSE">The MIT License</a></p>
-      </div>
-    </footer>
+<footer class='footer'>
+  <div class='container-fluid'>
+    <hr>
+    <p class='pull-right'>Thank you very much to <a href='http://derekeder.com'>Derek Eder</a>,
+    creator of <a href='https://github.com/derekeder/csv-to-html-table'>CSV to HTML Table</a>,
+    who released it under <a href="https://github.com/derekeder/csv-to-html-table/blob/master/LICENSE">The MIT License</a></p>
+  </div>
+</footer>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/jquery.csv.min.js"></script>
-    <script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="js/dataTables.bootstrap.js"></script>
-    <script type="text/javascript" src="js/csv_to_html_table.js"></script>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.csv.min.js"></script>
+<script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="js/dataTables.bootstrap.js"></script>
+<script type="text/javascript" src="js/csv_to_html_table.js"></script>
 
 
-    <script type="text/javascript">
-      function format_link(link){
-        if (link)
-          return "<a href='" + link + "' target='_blank'>" + link + "</a>";
-        else
-          return "";
-      }
+<script type="text/javascript">
+  function format_link(link){
+    if (link)
+      return "<a href='" + link + "' target='_blank'>" + link + "</a>";
+    else
+      return "";
+  }
 
-      CsvToHtmlTable.init({
-        csv_path: '$FULLNAMELOG',
-        element: 'table-container',
-        allow_download: true,
-        csv_options: {separator: ',', delimiter: '"'},
-        datatables_options: {"paging": false},
-        custom_formatting: [[3, format_link]]
-      });
-    </script>
-  </body>
+  CsvToHtmlTable.init({
+    csv_path: '$FULLNAMELOG',
+    element: 'table-container',
+    allow_download: true,
+    csv_options: {separator: ',', delimiter: '"'},
+    datatables_options: {"paging": true},
+    custom_formatting: [[3, format_link]]
+  });
+</script>
+</body>
 </html>
-
 _EOF_
+
+sed -i 's#<ul>#<ul>\n<li><a href="logs/$2">$2</a></li>#g' $LOGLIST
