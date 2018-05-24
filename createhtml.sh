80c4548a1b372406167fc70cf59aa005cb00d34e
ivanhercaz <ivan@ivanhercaz.com>
2018-05-24 09:09:01 +0100

    unnecessary conditional styles for Internet Explorer

--- createhtml.sh
+++ createhtml.sh
@@ -20,12 +20,6 @@ cat > $HTMLFILE << _EOF_
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/dataTables.bootstrap.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script type="text/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
