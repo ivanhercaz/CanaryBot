822577f5f4e21aaa2eb38dd53ca51d1307cbc931
CanaryBot <ivan@ivanhercaz.com>
2019-01-14 01:31:51 +0000

    migration of the Utilities class to utils.py (#8)

---
+++ utils.py
@@ -0,0 +1,60 @@
+import inquirer
+
+''' Class for useful snippets '''
+
+def editGroups(self):
+    ''' Method to check if the operator wants to create a set of changes in
+    Edit groups. Check: https://www.wikidata.org/wiki/Wikidata:Edit_groups '''
+
+    question = [
+        inquirer.Confirm("editGroups",
+            message="Do you want to track edits in a set of Edit groups?",
+            )
+    ]
+
+    answer = inquirer.prompt(question)
+
+    if answer["editGroups"] is True:
+        editGroup = "{:x}".format(random.randrange(0, 2**48))
+        return editGroup
+
+def editMode(self):
+    ''' Method to work like an easy and clean way to choose the edition mode,
+    otherwhise this could would have in each one '''
+
+    editMode = [
+        inquirer.List("editMode",
+            message="Elige el modo de edición",
+            choices=["Modo de pruebas", "Modo editar"],
+        ),
+    ]
+
+    answers = inquirer.prompt(editMode)
+
+    if answers["editMode"] == u"Modo editar":
+        edit = True
+        return edit
+
+def checkQueries(self):
+    f = []
+
+    for (dirs, dirsNames, files) in os.walk("queries"):
+        f.extend(files)
+        for filename in enumerate(files):
+            filenames = filename
+            print(filenames)
+
+    print("Hay {} consultas disponibles.\n".format(len(files)))
+
+    queries = [
+        inquirer.List("queries",
+            message="Qué consulta quieres utilizar?",
+            choices=files
+        ),
+    ]
+
+    queriesAnswer = inquirer.prompt(queries)
+    queriesAnswer = str(queriesAnswer)
+    queriesAnswer = queriesAnswer[13:].strip("'}")
+
+    return queriesAnswer
